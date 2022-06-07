e4403adbea1f370ac1708bf17cabcb0a1f194e08
blackarmVR <77742505+blackarmVR@users.noreply.github.com>
2022-06-07 13:39:47 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,34 @@
+sudo apt update && apt upgrade
 
+
+#install chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo add-apt-repository "deb http://dl.google.com/linux/chrome/deb/ stable main"
+sudo apt update
+sudo apt install -y google-chrome-stable
+
+#install codium
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
+    | sudo tee /etc/apt/sources.list.d/vscodium.list
+sudo apt update && sudo apt install -y codium
+
+#install spotify
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get install -y spotify-client
+
+sudo apt install -y blender
+sudo apt install -y gimp
+sudo apt install -y htop
+sudo apt install -y inkscape
+sudo apt install -y nginx
+sudo apt install -y openssh-server
+sudo apt install -y steam
+sudo apt install -y virtualbox
+sudo apt install -y whatsapp-desktop
+
+# open the page for node installation
+google-chrome https://github.com/nodesource/distributions/blob/master/README.md
